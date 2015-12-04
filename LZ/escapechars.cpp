9a69a4e8175e6f373ae9e9dfe17250323e92d8f3
FunkyBookends <caspar.nonclercq@gmail.com>
2015-12-04 10:44:20 +0000

    first implementation, doesn nto work

--- LZ/escapechars.cpp
+++ LZ/escapechars.cpp
@@ -1,6 +1,32 @@
-#include "escapechar.hpp"
+#include <iostream>
 
-std::string escapeChar(char ch){
+using namespace std;
+
+string escapeChar(char ch){
 	//takes in a charecter and returns a string. In most cases the returned string is simply the one character string containing ch, However escpaed chars are returned as the representation of them i.e. char('\t') returns string("\t").
 	
+	switch (ch){
+		case '\t':
+			return "\\t";
+			break;
+		case '\n':
+			return "\\n";
+			break;
+		case ' ':
+			return "\\s";
+			break;
+		case '\r':
+			return "\\r";
+			break;
+		default:
+			return string(1,ch);			
+	}
+}
+
+int main()
+{
+	char in;
+	while (cin >> in){
+		cout << escapeChar(in) << flush;
+	}
 }
