f0abb9d01f00fcc6679771e4ddcac79ed28a446d
FunkyBookends <caspar.nonclercq@gmail.com>
2015-12-04 10:21:23 +0000

    started task 1, prototyped function with comment

---
+++ LZ/escapechars.cpp
@@ -0,0 +1,6 @@
+#include "escapechar.hpp"
+
+std::string escapeChar(char ch){
+	//takes in a charecter and returns a string. In most cases the returned string is simply the one character string containing ch, However escpaed chars are returned as the representation of them i.e. char('\t') returns string("\t").
+	
+}
