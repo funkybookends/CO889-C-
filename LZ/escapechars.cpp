7bfcbc09101c0930a85a993adfa5b049fc395dba
FunkyBookends <caspar.nonclercq@gmail.com>
2015-12-05 21:20:15 +0000

    another attempt

--- LZ/escapechars.cpp
+++ LZ/escapechars.cpp
@@ -6,17 +6,18 @@ string escapeChar(char ch){
 	//takes in a charecter and returns a string. In most cases the returned string is simply the one character string containing ch, However escpaed chars are returned as the representation of them i.e. char('\t') returns string("\t").
 	
 	switch (ch){
-		case '\t':
-			return "\\t";
+		case 9:
+			return string("\\")+string("t");
 			break;
-		case '\n':
-			return "\\n";
+		case 10:
+			return string("\\")+string("n");
 			break;
-		case ' ':
-			return "\\s";
+		case 32:
+			cout<<"case space";
+			return string("\\")+string("s");
 			break;
-		case '\r':
-			return "\\r";
+		case 13:
+			return string("\\")+string("r");
 			break;
 		default:
 			return string(1,ch);			
