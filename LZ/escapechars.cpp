b1d841ba933a3cb7b5f4c7570e954c3199c16e49
FunkyBookends <caspar.nonclercq@gmail.com>
2015-12-05 21:48:00 +0000

    made it work, add in case for digits, use noskipws to make sure whitespace is read in properly

--- LZ/escapechars.cpp
+++ LZ/escapechars.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 string escapeChar(char ch){
 	//takes in a charecter and returns a string. In most cases the returned string is simply the one character string containing ch, However escpaed chars are returned as the representation of them i.e. char('\t') returns string("\t").
-	
+
 	switch (ch){
 		case 9:
-			return string("\\")+string("t");
+			return string("\\t");
 			break;
 		case 10:
-			return string("\\")+string("n");
+			return string("\\n");
 			break;
 		case 32:
-			cout<<"case space";
-			return string("\\")+string("s");
+			return string("\\s");
 			break;
 		case 13:
-			return string("\\")+string("r");
+			return string("\\r");
 			break;
 		default:
+			if (ch>=48 && ch<=57) {
+				return string("\\") + string(1, ch);
+			}
 			return string(1,ch);			
 	}
 }
@@ -27,7 +30,7 @@ string escapeChar(char ch){
 int main()
 {
 	char in;
-	while (cin >> in){
-		cout << escapeChar(in) << flush;
+	while (cin >> noskipws >> in){
+		cout << escapeChar(in);
 	}
 }
